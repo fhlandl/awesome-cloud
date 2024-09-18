37b1b096089c3f8898d1d2bae59f24f295825fe9
fhlandl <mkj1206@gmail.com>
2024-09-18 17:33:51 +0900

    Add SNB Example
    - Implement simple SNB for example

--- src/components/nav/SNB.tsx
+++ src/components/nav/SNB.tsx
@@ -4,7 +4,12 @@ import styles from './SNB.module.scss';
 const SNB = () => {
   return (
     <aside className={styles.aside}>
-      <nav className={styles.nav}>SNB</nav>
+      <nav className={styles.nav}>
+        <div>Item Example 1</div>
+        <div>Item Example 2</div>
+        <div>Item Example 3</div>
+        <div>Item Example 4</div>
+      </nav>
     </aside>
   );
 };
