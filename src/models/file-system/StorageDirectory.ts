0a75eee4b212701aa01a0aa89e0b9bb2bad1f125
fhlandl <mkj1206@gmail.com>
2024-09-27 09:14:17 +0900

    Add storage test example
    - by vitest

--- src/models/file-system/StorageDirectory.ts
+++ src/models/file-system/StorageDirectory.ts
@@ -1,17 +1,17 @@
 import StorageFile from './StorageFile';
 import StorageNode from './StorageNode';
 
-class Directory extends StorageNode {
-  private children: (Directory | StorageFile)[];
+class StorageDirectory extends StorageNode {
+  private children: (StorageDirectory | StorageFile)[];
 
   constructor(name: string, parent: StorageNode | null) {
     super(name, parent);
     this.children = [];
   }
 
-  public add(child: Directory | StorageFile) {
+  public add(child: StorageDirectory | StorageFile) {
     this.children.push(child);
   }
 }
 
-export default Directory;
+export default StorageDirectory;
